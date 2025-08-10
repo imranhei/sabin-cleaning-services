890731228eb4446e85061f30ebe1fcc1fbacfe4e
Imran Hossain <imranhossainrana.2014@gmail.com>
2025-08-10 20:13:02 +0600

    edit blog page, inbox note for admin and breadcrumb for blod details page

--- client/src/components/common/RenderBreadcrumb.jsx
+++ client/src/components/common/RenderBreadcrumb.jsx
@@ -10,7 +10,6 @@ import {
 } from "@/components/ui/breadcrumb"; // adjust path if needed
 
 const RenderBreadcrumb = ({ items }) => {
-  console.log(items);
   return (
     <Breadcrumb>
       <BreadcrumbList>
